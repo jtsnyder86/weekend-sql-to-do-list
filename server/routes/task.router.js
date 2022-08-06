1e60b4be3949865724b92aaf2182f6d29fabe222
Josh Snyder <joshua.t.snyder@gmail.com>
2022-08-06 15:23:52 -0500

    added GET in router

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -1,9 +1,35 @@
+const { query } = require('express');
 const express = require ('express');
 
 const taskRouter = express.Router ();
 
-// GET
+// DB connection
+const pg = require ('pg');
+const Pool = pg.Pool;
 
+
+const pool = new Pool({
+    database: 'weekend-to-do-app',
+    host: 'localhost',
+    port: 5432
+});
+
+
+
+// GET
+taskRouter.get('/', (req, res) => {
+    let queryText = `
+    SELECT * FROM "todo";`;
+
+    pool.query(queryText)
+        .then( result => {
+            console.log(result.rows);
+            res.send(result.rows)
+        }).catch( err => {
+            console.log(err);
+            res.sendStatus(500)
+        });
+});
 
 // POST
 
