ec0b3e8b8353f9d55534e4146bd910c838e8aa36
Josh Snyder <joshua.t.snyder@gmail.com>
2022-08-06 16:14:28 -0500

    added append to getTasks function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -16,4 +16,22 @@ function getTasks () {
     console.log('in getTasks');
     $('#viewTasks').empty();
 
-}
+    $.ajax({
+        method: 'GET',
+        url: '/task'
+    }).then( function (response){
+        console.log('GET my tasks', response);
+
+        for (task of response){
+            console.log(task);
+            $('#viewTasks').append(`
+            <tr data-id = ${task.id}>
+                <td>${task.task}</td>
+                <td>${task.status}</td>
+                <td><button class = "completeBtn">COMPLETE</button></td>
+                <td><button class = "deleteBtn">DELETE</button></td>
+            </tr>
+            `)
+        }
+    })
+};
