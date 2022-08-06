5bf1c3b93fb8b47b63e5e528418e48c1758ff047
Josh Snyder <joshua.t.snyder@gmail.com>
2022-08-06 13:46:49 -0500

    got all of the files created and dependencies installed

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "todo" (
+    "id" SERIAL PRIMARY KEY,
+    "task" VARCHAR (200) NOT NULL,
+    "status" BOOLEAN
+);
+
+INSERT INTO
+    "todo" ("task", "status")
+VALUES
+    ('Take the dogs for a walk', true),
+    ('Take the dogs for another walk', false),
+    ('Run the dishwasher', false),
+    ('Vacuum the house', true);
---
+++ server/public/scripts/client.js
@@ -0,0 +1,7 @@
+console.log('script running');
+
+$(hanldeReady);
+
+function handleReady () {
+    console.log('jq running');
+};
