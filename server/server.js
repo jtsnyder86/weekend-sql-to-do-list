a2e417701c264be1ca8583b9cf4b94713666835f
Josh Snyder <joshua.t.snyder@gmail.com>
2022-08-07 11:58:01 -0500

    started testing bootstrap

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-const express = require ('express');
+const express = require('express');
 
-const app = express ();
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('server/public'));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-const taskRouter = require ('./routes/task.router');
+const taskRouter = require('./routes/task.router');
 app.use('/task', taskRouter);
 
 
